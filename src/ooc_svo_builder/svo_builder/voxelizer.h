226db5bbdd78d4f8ac7e440fdea6c29bac665819
Mark Kim <mbk@cs.utah.edu>
2015-03-12 13:27:10 -0600

    Rearrange the CPU code to operate like the GPU. That means find out the memory size of data, then run and fill data.

--- src/ooc_svo_builder/svo_builder/voxelizer.h
+++ src/ooc_svo_builder/svo_builder/voxelizer.h
@@ -18,22 +18,12 @@ using namespace std;
 #define FULL_VOXEL 1
 #define WORKING_VOXEL 2
 
-#ifdef BINARY_VOXELIZATION
-void voxelize_huang_method(TriReader &reader, const uint64_t morton_start, const uint64_t morton_end, const float unitlength, bool* voxels, size_t &nfilled);
-#else
-void voxelize_huang_method(TriReader &reader, const uint64_t morton_start, const uint64_t morton_end, const float unitlength, size_t* voxels, vector<VoxelData>& voxel_data, size_t &nfilled);
-#endif
+
 
 #ifdef BINARY_VOXELIZATION
 void voxelize_schwarz_method(TriReaderIter &reader, const uint64_t morton_start, const uint64_t morton_end, const float unitlength, tbb::atomic<char>* voxels, tbb::concurrent_vector<uint64_t> &data, float sparseness_limit, bool &use_data, tbb::atomic<size_t> &nfilled);
 #else
 void voxelize_schwarz_method(TriReaderIter &reader, const uint64_t morton_start, const uint64_t morton_end, const float unitlength, tbb::atomic<char>* voxels, tbb::concurrent_vector<VoxelData> &data, float sparseness_limit, bool &use_data, tbb::atomic<size_t> &nfilled);
 #endif
-
-//#ifdef BINARY_VOXELIZATION
-//void voxelize_partition3(TriReader &reader, const uint64_t morton_start, const uint64_t morton_end, const float unitlength, char* voxels, vector<uint64_t> &data, float sparseness_limit, bool &use_data, size_t &nfilled);
-//#else
-//void voxelize_partition3(TriReader &reader, const uint64_t morton_start, const uint64_t morton_end, const float unitlength, char* voxels, vector<uint64_t> &data, float sparseness_limit, bool &use_data, size_t &nfilled);
-//#endif
 
 #endif // VOXELIZER_H_
